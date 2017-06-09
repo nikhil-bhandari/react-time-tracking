304947cde30d533f408c81a036c4ca25f7d7d674
Nikhil Bhandari <nikhil.bhandari@intelligrape.com>
2017-06-10 03:22:56 +0530

    Added bolierplate for alll components

--- public/app.js
+++ public/app.js
@@ -1,1 +1,35 @@
+const TimersDashboard = React.createClass({
+    render: function () {
 
+    }
+});
+
+const EditableTimerList = React.createClass({
+    render: function () {
+
+    }
+});
+
+const EditableTimer = React.createClass({
+    render: function () {
+
+    }
+});
+
+const Timer = React.createClass({
+    render: function () {
+
+    }
+});
+
+const TimerForm = React.createClass({
+    render: function () {
+
+    }
+});
+
+const ToggleableTimerForm = React.createClass({
+    render: function () {
+
+    }
+});
